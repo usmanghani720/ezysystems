27585423ecdfd08f2fdd6fe0163162305c7acb3b
usmanghani720 <usman.ghani720@gmail.com>
2025-12-10 16:26:17 +0500

    fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,24 @@
 
 #DailyBillingJob.perform_now()
 
-User.where.not(stripe_user_id: nil).each do |user|
+# User.where.not(stripe_user_id: nil).each do |user|
+#     @code = rand(8 ** 8)
+#     user.update(unique_code: @code)
+#     if user.role != "admin"
+#         user.update(role: 'vendor')
+#     end
+# end
+
+
+User.all.each do |user|
     @code = rand(8 ** 8)
     user.update(unique_code: @code)
     if user.role != "admin"
         user.update(role: 'vendor')
     end
+    if user.role == "vendor" && user.unique_code.blank?
+        user.update(unique_code: @code)
+    end
 end
+
+
