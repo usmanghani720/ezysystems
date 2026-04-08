cd483573efec58bdbf25bf15f5c00d2464c7fec8
usmanghani720 <usman.ghani720@gmail.com>
2026-04-08 14:34:06 +0500

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method = :smtp
 
+  config.enable_reloading = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
